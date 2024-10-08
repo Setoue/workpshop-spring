12f0d580e2a67245fb7afb808b7c802a866ea54c
Setoue <proshigeru@gmail.com>
2024-10-07 22:39:56 -0300

    feat: add service layer, component registration

---
+++ src/main/java/com/shigeru/app_spring/services/UserService.java
@@ -0,0 +1,25 @@
+package com.shigeru.app_spring.services;
+
+import com.shigeru.app_spring.entities.User;
+import com.shigeru.app_spring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}

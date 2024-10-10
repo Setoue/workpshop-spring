d9549061e920950e2ce51d4996245e638ad2fdac
Setoue <proshigeru@gmail.com>
2024-10-10 00:15:48 -0300

    feat: add OrderStatus Enum

--- src/main/java/com/shigeru/app_spring/config/TestConfig.java
+++ src/main/java/com/shigeru/app_spring/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.shigeru.app_spring.config;
 
 import com.shigeru.app_spring.entities.Order;
 import com.shigeru.app_spring.entities.User;
+import com.shigeru.app_spring.entities.enums.OrderStatus;
 import com.shigeru.app_spring.repositories.OrderRepository;
 import com.shigeru.app_spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,10 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-        Order o4 = new Order(null, Instant.parse("2019-07-22T15:21:21Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
+        Order o4 = new Order(null, Instant.parse("2019-07-22T15:21:21Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3, o4));
--- src/main/java/com/shigeru/app_spring/entities/Order.java
+++ src/main/java/com/shigeru/app_spring/entities/Order.java
@@ -1,7 +1,7 @@
 package com.shigeru.app_spring.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.shigeru.app_spring.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -22,16 +22,19 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {};
 
-    public Order(Long id, Instant moment, User client){
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client){
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -57,6 +60,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
